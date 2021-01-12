fdea271f02a3391f8ac95c8d64ed0c35eaad6f06
tapas1000 <tapas@swarmlabs.io>
2021-01-12 11:08:38 +0530

    added log

--- src/want-manager/index.js
+++ src/want-manager/index.js
@@ -67,7 +67,16 @@ module.exports = class WantManager {
   }
 
   _addEntries(cids, cancel, force) {
-    console.log("ADD ENTRIES CIDD LENGHT", cids.length)
+    console.log("ADD ENTRIES CIDD LENGHT", cids, cids.length,cancel);
+    if(cids.length > 0) {
+      for(let cid of cids){
+        if(cid.length === 0){
+          cids.splice(cids.indexOf(cid));
+        }
+      }
+      console.log('after filter cids',cids);
+      if(cids.length === 0) return;
+    }
     const entries = cids.map((cid, i) => {
       return new Message.Entry(
         cid,
@@ -95,113 +104,103 @@ module.exports = class WantManager {
     let $peers = Array.from(this.peers.values());
     console.log("all peers", $peers, entries, parseInt(new Date().getTime()/ 1000));
 
-    this.p = $peers[Math.floor(Math.random() * $peers.length)];
-    console.log("THIS.P", this.p)
-    this.p.addEntries(entries)
-    // if(this.availablePeers.length === 0){
-    //   this.availablePeers = [...$peers];
-    //   this.busyPeers = new Map();
-    // }
+    // this.p = $peers[Math.floor(Math.random() * $peers.length)];
+    // console.log("THIS.P", this.p)
+    // this.p.addEntries(entries)
+    if(this.availablePeers.length === 0){
+      this.availablePeers = [...$peers];
+      this.busyPeers = new Map();
+    }
 
-    // if (this.availablePeers.length > 0) {
-    //   if (entries.length > 0) {
-    //     console.log('already asent request',!this.sentRequests.has(entries[0].entry.cid.string),JSON.stringify(entries),this.sentRequests,entries[0].entry.cid.string)
-    //     if(entries[0].entry.cid.string && this.sentRequests.has(entries[0].entry.cid.string) && entries[0].cancel ) {
-    //       let requestSentToPeers = this.sentRequests.get(entries[0].entry.cid.string)
-    //       console.log("selected peer cancel", requestSentToPeers.peers, entries, parseInt(new Date().getTime()/ 1000));
+    if (this.availablePeers.length > 0) {
+      if (entries.length > 0) {
+        let entryString = entries[0].entry.cid.string;
+        if(!entryString){
+          entryString = entries[0].entry.cid.toString();
+        }
+        console.log('selected peer intial',this.sentRequests.has(entryString), entries[0].cancel, entries.toString())
+        if(entryString && this.sentRequests.has(entryString) && entries[0].cancel ) {
+          let requestSentToPeers = this.sentRequests.get(entryString)
+          console.log("selected peer cancel", requestSentToPeers.peer, entries.toString(), parseInt(new Date().getTime()/ 1000));
 
-    //       for(let i of requestSentToPeers.peers){
-    //         i.addEntries(entries)
-    //       }
-
-    //       this.sentRequests.delete(entries[0].entry.cid.string)
-    //     }
-    //     else if(entries[0].entry.cid.string && !this.sentRequests.has(entries[0].entry.cid.string)){
-    //       console.log("AVAILABLE PEERS", this.availablePeers, this.busyPeers, parseInt(new Date().getTime()/ 1000));
-    //       let tempPeer;
          
-    //       this.p = this.availablePeers.shift();
-    //       this.sentRequests.set(entries[0].entry.cid.string, {peers: [this.p]})
-    //       console.log("selected peer", this.p, entries, parseInt(new Date().getTime()/ 1000));
-    //       this.p.addEntries(entries);
-    //       if(this.busyPeers.keys().length > 0){
-    //         tempPeer = this.busyPeers.get(this.busyPeers.keys()[0]);
-    //         this.busyPeers.delete(this.busyPeers.keys()[0]);
-    //        }
-    //       this.busyPeers.set(this.p.peerId.toB58String(), {blockProcessing: true, addedAt: parseInt(new Date().getTime()/ 1000), peer: this.p});
-    //       if(tempPeer) this.availablePeers.push(tempPeer.peer);
+          requestSentToPeers.peer.addEntries(entries)
 
-    //     } 
+          this.sentRequests.delete(entryString)
+        }
+        else if(entryString && !this.sentRequests.has(entryString) && !entries[0].cancel){
+          console.log("AVAILABLE PEERS", this.availablePeers, this.busyPeers, parseInt(new Date().getTime()/ 1000));
+          let tempPeer;
+         
+          this.p = this.availablePeers.shift();
+          this.sentRequests.set(entryString, {peer: this.p})
+          console.log("selected peer sent", this.p, entries.toString(), parseInt(new Date().getTime()/ 1000));
+          this.p.addEntries(entries);
+          if(this.busyPeers.keys().length > 0){
+            tempPeer = this.busyPeers.get(this.busyPeers.keys()[0]);
+            this.busyPeers.delete(this.busyPeers.keys()[0]);
+           }
+          this.busyPeers.set(this.p.peerId.toB58String(), {blockProcessing: true, addedAt: parseInt(new Date().getTime()/ 1000), peer: this.p});
+          if(tempPeer) this.availablePeers.push(tempPeer.peer);
+
+        } 
           
-    //     else{
-    //       console.log('no condition satisfied');
-    //     }
-    //     // }
-    //     // if (
-    //     //   this.prevEntries &&
-    //     //   entries &&
-    //     //   this.prevEntries[0] &&
-    //     //   entries[0] &&
-    //     //   entries[0].entry.cid.string !== this.prevEntries[0].entry.cid.string
-    //     // ) {
+        else{
+          console.log('no condition satisfied',entries[0].entry.cid);
+          console.log('no function',entries[0].entry.cid.toString());
+        }
+        if (this.interval) clearInterval(this.interval);
+        this.interval = setInterval(() => {
+          if(this.availablePeers.length === 0){
+            console.log('return due to no available peers');
+            return
+          }          
+          if(entryString && this.sentRequests.has(entryString)) {
+            let requestSentTo = this.sentRequests.get(entryString)  
+            console.log('selected peer add cancel request',requestSentTo.peer,entries.toString());
+      
+            
+
+            let fallbackentries = []
+            for (let ele of entries){
+              if(!ele.cancel) {
+                ele.cancel = true
+                fallbackentries.push(ele)
+
+              }
+
+            }
+            // console.log("FALL")
+            if(fallbackentries && fallbackentries.length>0) {
+              requestSentTo.peer.addEntries(fallbackentries);
+
+            
+              console.log("AVAILABLE PEERS", this.availablePeers, this.busyPeers, "requestSentTo", requestSentTo, parseInt(new Date().getTime()/ 1000));
+              let rPeers = this.availablePeers.filter((ele) => ele !== requestSentTo.peer)
+              console.log("entries second condition", entries, this.prevEntries, rPeers, parseInt(new Date().getTime()/ 1000));
+
+              if(rPeers && rPeers.length > 0) { 
+                this.p = rPeers.shift();
+    
+                console.log("selected peer fallback", this.p, entries.toString(), fallbackentries.toString(), parseInt(new Date().getTime()/ 1000));
+                this.p.addEntries(entries);
+                this.busyPeers.set(this.p.peerId.toB58String(), {blockProcessing: true, addedAt: parseInt(new Date().getTime()/ 1000), peer: this.p});
+                this.sentRequests.set(entryString, {peer:  this.p})
+              }
           
-    //     //   console.log("AVAILABLE PEERS", this.availablePeers, this.busyPeers, parseInt(new Date().getTime()/ 1000));
+              else{
+                console.log('rpeers not defined or blank array')
+              }
+            }
+          }
+          else{
 
-    //     //   this.p = this.availablePeers[Math.floor(Math.random() * this.availablePeers.length)];
-    //     //   console.log("selected peer", this.p, entries, parseInt(new Date().getTime()/ 1000));
-    //     //   this.p.addEntries(entries);
-    //     //   this.busyPeers.set(this.p.peerId.toB58String(), {blockProcessing: true, addedAt: parseInt(new Date().getTime()/ 1000)})
-    //     // }
+          console.log('wasted one request', entryString, entryString);  
 
-    //     if (this.interval) clearInterval(this.interval);
-    //     this.interval = setInterval(() => {
-    //       if(this.availablePeers.length === 0){
-    //         console.log('return due to no available peers');
-    //         return
-    //       }
-
-          
-    //       if(entries[0].entry.cid.string && this.sentRequests.has(entries[0].entry.cid.string)) {
-    //         if (
-    //           this.prevEntries.length === 1 &&
-    //           entries.length === 1 &&
-    //           this.prevEntries[0] &&
-    //           entries[0] &&
-    //           entries[0].entry.cid.string === this.prevEntries[0].entry.cid.string &&
-    //           !entries[0].cancel
-    //         ) {
-              
-    //           let requestSentTo = this.sentRequests.get(entries[0].entry.cid.string)
-    //           console.log("AVAILABLE PEERS", this.availablePeers, this.busyPeers, "requestSentTo", requestSentTo, parseInt(new Date().getTime()/ 1000));
-  
-    //           // clearTimeout(interval);
-    //           // this.p = $peers[Math.floor(Math.random() * $peers.length)];
-    //           let rPeers = this.availablePeers.filter((ele) => !requestSentTo.peers.includes(ele))
-    //           console.log("entries second condition", entries, this.prevEntries, rPeers, parseInt(new Date().getTime()/ 1000));
-
-    //          if(rPeers && rPeers.length > 0) { 
-    //           this.p = rPeers.shift();
-  
-    //           console.log("selected peer", this.p, entries, parseInt(new Date().getTime()/ 1000));
-    //           this.p.addEntries(entries);
-    //           // this.p.
-    //           this.busyPeers.set(this.p.peerId.toB58String(), {blockProcessing: true, addedAt: parseInt(new Date().getTime()/ 1000), peer: this.p});
-    //           this.sentRequests.set(entries[0].entry.cid.string, {peers: [...requestSentTo.peers, this.p]})
-    //         }
-    //         else{
-    //           console.log('rpeers not defined or blank array')
-    //         }
-    //         }
-    //       }
-
-    //       else{
-
-    //       console.log('wasted one request');  
-
-    //       }
-    //     }, retryTime * 1000);
-    //   }
-    // }
+          }
+        }, retryTime * 1000);
+      }
+    }
 
     // if (entries) {
     //   this.prevEntries = [...entries];
@@ -257,18 +256,20 @@ module.exports = class WantManager {
 
   // add all the cids to the wantlist
   wantBlocks(cids, options = {}) {
-    // this._addEntries(cids, false);
-    this.storedCIDS = [...this.storedCIDS,...cids]
-    console.log("INSIDE WANT BLOCK", cids, "ssssssssssss", this.storedCIDS)
-    if(this.root && this.storedCIDS.length === 2) {
-      this._addEntries(this.storedCIDS, false);
-      this.storedCIDS = []
-    }
-    if(!this.root){
-      this._addEntries(cids, false);
-      this.root = true;
-      this.storedCIDS = []
-    }
+
+    console.log('cids want block',cids);
+    this._addEntries(cids, false);
+    // this.storedCIDS = [...this.storedCIDS,...cids]
+    // console.log("INSIDE WANT BLOCK", cids, "ssssssssssss", this.storedCIDS)
+    // if(this.root && this.storedCIDS.length === 2) {
+    //   this._addEntries(this.storedCIDS, false);
+    //   this.storedCIDS = []
+    // }
+    // if(!this.root){
+    //   this._addEntries(cids, false);
+    //   this.root = true;
+    //   this.storedCIDS = []
+    // }
 
     if (options && options.signal) {
       options.signal.addEventListener("abort", () => {
@@ -285,8 +286,12 @@ module.exports = class WantManager {
 
   // cancel wanting all of the given keys
   cancelWants(cids) {
+    if(!cids.length && typeof(cids) === 'object') cids = [cids];
     this._log("cancel wants: %s", cids.length);
-    this._addEntries(cids, true);
+    if(cids.length > 0){
+       console.log('cids cancelwant',cids)
+       this._addEntries(cids, true);
+    }
   }
 
   // Returns a list of all currently connected peers
